4ac54df8fc3da8629e527809e934151266cbf813
Rafael Morales <rmorales@utem.cl>
2022-11-30 23:05:41 -0300

    Eval mas generico, mas evaluaciones

--- main.py
+++ main.py
@@ -41,7 +41,10 @@ def op_time(row):
 def evaluate_column(cell, hay, exact = True):
     try:
         if(exact):
-            return hay.index(cell) + 1
+            for index, value in enumerate(hay):
+                if(cell == value or cell in value):
+                    return index + 1
+            return 0
         else:
             for index, word in enumerate(hay):
                 if(word in cell):
@@ -62,6 +65,29 @@ data['eval_eof'] = data.apply(lambda x: evaluate_column(x['Extent_Of_Fire'], [
     'Spread',
     'Entire',
     ], exact=False), axis=1)
+
+
+data['eval_possible_cause'] = data.apply(lambda x: evaluate_column(x['Possible_Cause'], [
+    ['Undetermined', 'Unintentional, cause undetermined', 'Other'],
+    ['Improperly Discarded', 'Other unintentional cause, not classified'],
+    ['Improper handling of ignition source or ignited material', 'Routine maintenance deficiency, eg creosote, lint, grease buildup'],
+    ['Electrical Failure', 'Design/Construction/Installation/Maintenance Deficiency'],
+    ['Unattended', 'Used or Placed too close to combustibles'],
+    ]), axis=1)
+
+data['eval_sofoa'] = data.apply(lambda x: evaluate_column(x['Status_of_Fire_On_Arrival'], [
+['Fire extinguished prior to arrival', 'Unclassified'],
+['Fire with no evidence from street', 'Fire with smoke showing only - including vehicle, outdoor fires'],
+'Flames showing from small area (one storey or less, part of a vehicle, outdoor)',
+'Flames showing from large area (more than one storey, large area outdoors)',
+['Exposure involved', 'Fully involved (total structure, vehicle, spreading outdoor fire)'],
+    ]), axis=1)
+
+data['eval_faso'] = data.apply(lambda x: evaluate_column(x['Fire_Alarm_System_Operation'], [
+    'Fire alarm system operated',
+    'Fire alarm system operation undetermined',
+    ['Not applicable (no system)', 'Fire alarm system did not operate']
+    ]), axis=1)
 
 
 from pandas_profiling import ProfileReport
